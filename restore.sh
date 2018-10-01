e2d83d880d55bbe92ebfef38aa9dfeae5e925583
otrishyn <alexey_trishin@bintime.com>
2018-10-01 10:57:59 +0300

    Fix scripts

--- restore.sh
+++ restore.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-export PROVISIONING_PATH='/provisioning/provisioning'
-docker-compose exec mysql-server bash -c "cd ${PROVISIONING_PATH}; ./mysql-restore.sh futurum"
-docker-compose exec mysql-server bash -c "cd ${PROVISIONING_PATH}; ./mysql-restore.sh futurum_cc"
+docker-compose exec mysql-server bash -c "cd ${PROVISIONING_PATH:-provisioning}; cd dump/`date +\%Y-\%m-\%d`; ../../mysql-restore.sh futurum"
+docker-compose exec mysql-server bash -c "cd ${PROVISIONING_PATH:-provisioning}; cd dump/`date +\%Y-\%m-\%d`; ../../mysql-restore.sh futurum_cc"
 
-docker-compose exec mongo-server26 bash -c "cd ${PROVISIONING_PATH}; ./mongo-restore.sh futurum"
-docker-compose exec mongo-server26 bash -c "cd ${PROVISIONING_PATH}; ./mongo-restore.sh futurum_cc"
+docker-compose exec mongo-server26 bash -c "cd ${PROVISIONING_PATH:-provisioning}; cd dump/`date +\%Y-\%m-\%d`; sh ../../mongo-restore.sh futurum"
+docker-compose exec mongo-server26 bash -c "cd ${PROVISIONING_PATH:-provisioning}; cd dump/`date +\%Y-\%m-\%d`; sh ../../mongo-restore.sh futurum_cc"
