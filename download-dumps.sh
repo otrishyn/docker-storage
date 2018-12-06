ec4839bbc5a85563b7fb57e54bda5a85d878e8b6
otrishyn <alexey_trishin@bintime.com>
2018-12-06 11:48:10 +0200

    Fix dump downloader

--- download-dumps.sh
+++ download-dumps.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 export DESTINATION=${PROVISIONING_PATH:-$HOME/Code/provisioning}/dump/;
-mkdir -p $DESTINATION; rsync -vazuP testing:/data/www/dump/*${DATE:-`date +\%Y-\%m-\%d`}* $DESTINATION
+#mkdir -p $DESTINATION; rsync -vazuP testing:/data/www/dump/*${DATE:-`date +\%Y-\%m-\%d`}* $DESTINATION
+mkdir -p $DESTINATION; rsync -vazuP test1:/home/deployer/dump/*${DATE:-`date +\%Y-\%m-\%d`}* $DESTINATION
